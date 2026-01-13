2dffac81a5dac2f1c8b4b2d0f8dfe709f674cd03
Subhan Alom <subhanalombasic123@gmail.com>
2026-01-13 15:25:09 +0530

    setup vercel rewrites for unified routing

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
+      <Router basename="/clinic-admin">
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
