93575c0af50467f30dd4a0111c507ce92cfbe9ae
Subhan Alom <subhanalombasic123@gmail.com>
2026-01-13 14:06:43 +0530

    updated meta data

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 export default defineConfig({
+  base: "/clinic-admin/",
   plugins: [react()],
   optimizeDeps: { exclude: ["lucide-react"] },
   resolve: {
